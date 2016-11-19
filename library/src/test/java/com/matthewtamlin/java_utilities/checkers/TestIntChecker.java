91a89add51cb89a5ad3545beb23a5aa47bb6a23d
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-19 13:27:28 +1030

    Updated TestIntChecker.java
    - Added tests for the checkGreaterThanOrEqual method
    - Added testts for the checkLessThanOrEqualTo method

--- library/src/test/java/com/matthewtamlin/java_utilities/checkers/TestIntChecker.java
+++ library/src/test/java/com/matthewtamlin/java_utilities/checkers/TestIntChecker.java
@@ -31,101 +31,209 @@ public class TestIntChecker {
 	 * Failure message for use when the wrong integer is returned by the checker.
 	 */
 	private static final String WRONG_RETURN = "Checked passed but returned wrong value	";
-
+	
 	/**
-	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions correctly
-	 * when the {@code x} argument is less than the {@code y} argument. The test will only
-	 * pass if the correct integer is returned by the check and no exception is thrown.
+	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions
+	 * correctly when the {@code x} argument is less than the {@code y} argument. The test will
+	 * only pass if the correct integer is returned by the check and no exception is thrown.
 	 */
 	@Test
 	public void testCheckLessThan_xLessThanY() {
 		final int result = IntChecker.checkLessThan(5, 6);
 		assertThat(WRONG_RETURN, result == 5);
 	}
-
+	
 	/**
-	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions correctly
-	 * when the {@code x} argument is equal to the {@code y} argument. The test will only
+	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions
+	 * correctly when the {@code x} argument is equal to the {@code y} argument. The test will only
 	 * pass if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckLessThan_xEqualToY() {
 		IntChecker.checkLessThan(5, 5);
 	}
-
+	
 	/**
-	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions correctly
-	 * when the {@code x} argument is greater than the {@code y} argument. The test will
+	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions
+	 * correctly when the {@code x} argument is greater than the {@code y} argument. The test will
 	 * only pass if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckLessThan_xGreaterThanY() {
 		IntChecker.checkLessThan(6, 5);
 	}
-
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions
+	 * correctly when {@code x} is less than {@code y}, and {@code y} is equal to
+	 * {@link Integer#MAX_VALUE}. The test will only pass if the correct integer is returned by
+	 * the check and no exception is thrown.
+	 */
+	public void testCheckLessThan_yIsMaxValue() {
+		final int result = IntChecker.checkLessThan(6, Integer.MAX_VALUE);
+		assertThat(WRONG_RETURN, result == 6);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkLessThanOrEqualTo(int, int)} method functions
+	 * correctly when the {@code x} argument is less than the {@code y} argument. The test will
+	 * only pass if the correct integer is returned by the check and no exception is thrown.
+	 */
+	@Test
+	public void testCheckLessThanOrEqualTo_xLessThanY() {
+		final int result = IntChecker.checkLessThanOrEqualTo(5, 6);
+		assertThat(WRONG_RETURN, result == 5);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkLessThanOrEqualTo(int, int)} method functions
+	 * correctly when the {@code x} argument is equal to the {@code y} argument. The test will
+	 * only pass if the correct integer is returned by the check and no exception is thrown.
+	 */
+	@Test
+	public void testCheckLessThanOrEqualTo_xEqualToY() {
+		final int result = IntChecker.checkLessThanOrEqualTo(5, 5);
+		assertThat(WRONG_RETURN, result == 5);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkLessThanOrEqualTo(int, int)} method functions
+	 * correctly when the {@code x} argument is greater than the {@code y} argument. The test will
+	 * only pass if an IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckLessThanOrEqualTo_xGreaterThanY() {
+		IntChecker.checkLessThanOrEqualTo(6, 5);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkLessThan(int, int)} method functions
+	 * correctly when {@code x} is less than {@code y}, and {@code y} is equal to
+	 * {@link Integer#MAX_VALUE}. The test will only pass if the correct integer is returned by
+	 * the check and no exception is thrown.
+	 */
+	public void testCheckLessThanOrEqualTo_yIsMaxValue() {
+		final int result = IntChecker.checkLessThanOrEqualTo(6, Integer.MAX_VALUE);
+		assertThat(WRONG_RETURN, result == 6);
+	}
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkGreaterThan(int, int)} method functions
-	 * correctly when the {@code x} argument is less than the {@code y} argument. The
-	 * test will only pass if an IllegalArgumentException is thrown.
+	 * correctly when the {@code x} argument is less than the {@code y} argument. The test will
+	 * only pass if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckGreaterThan_xLessThanY() {
 		IntChecker.checkGreaterThan(5, 6);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkGreaterThan(int, int)} method functions
-	 * correctly when the {@code x} argument is equal to the {@code y} argument. The
-	 * test will only pass if an IllegalArgumentException is thrown.
+	 * correctly when the {@code x} argument is equal to the {@code y} argument. The test will
+	 * only pass if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckGreaterThan_xEqualToY() {
 		IntChecker.checkGreaterThan(5, 5);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkGreaterThan(int, int)} method functions
-	 * correctly when the {@code x} argument is greater than the {@code y} argument. The
-	 * test will only pass if the correct integer is returned by the check and no exception is
-	 * thrown.
+	 * correctly when the {@code x} argument is greater than the {@code y} argument. The test
+	 * will only pass if the correct integer is returned by the check and no exception is thrown.
 	 */
 	@Test
 	public void testCheckGreaterThan_xGreaterThanY() {
 		final int result = IntChecker.checkGreaterThan(6, 5);
 		assertThat(WRONG_RETURN, result == 6);
 	}
-
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkGreaterThan(int, int)} method functions
+	 * correctly when {@code x} is less than {@code y}, and {@code x} is equal to
+	 * {@link Integer#MIN_VALUE}. The test will only pass if the correct integer is returned by
+	 * the check and no exception is thrown.
+	 */
+	public void testCheckLessThan_yIsMinValue() {
+		final int result = IntChecker.checkGreaterThan(Integer.MIN_VALUE, 6);
+		assertThat(WRONG_RETURN, result == Integer.MIN_VALUE);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkGreaterThanOrEqualTo(int, int)} method
+	 * functions correctly when the {@code x} argument is less than the {@code y} argument. The
+	 * test will only pass if an IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckGreaterThanOrEqualTo_xLessThanY() {
+		IntChecker.checkGreaterThanOrEqualTo(5, 6);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkGreaterThanOrEqualTo(int, int)} method
+	 * functions correctly when the {@code x} argument is equal to the {@code y} argument. The test
+	 * will only pass if the correct integer is returned by the check and no exception is thrown.
+	 */
+	@Test
+	public void testCheckGreaterThanOrEqualTo_xEqualToY() {
+		final int result = IntChecker.checkGreaterThanOrEqualTo(5, 5);
+		assertThat(WRONG_RETURN, result == 5);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkGreaterThanOrEqualTo(int, int)} method
+	 * functions correctly when the {@code x} argument is greater than the {@code y} argument.
+	 * The test will only pass if the correct integer is returned by the check and no exception
+	 * is thrown.
+	 */
+	@Test
+	public void testCheckGreaterThanOrEqualTo_xGreaterThanY() {
+		final int result = IntChecker.checkGreaterThanOrEqualTo(6, 5);
+		assertThat(WRONG_RETURN, result == 6);
+	}
+	
+	/**
+	 * Test to verify that the {@link IntChecker#checkGreaterThanOrEqualTo(int, int)} method
+	 * functions correctly when {@code x} is less than {@code y}, and {@code x} is equal to
+	 * {@link Integer#MIN_VALUE}. The test will only pass if the correct integer is returned by
+	 * the check and no exception is thrown.
+	 */
+	public void testCheckLessThanOrEqualTo_yIsMinValue() {
+		final int result = IntChecker.checkGreaterThanOrEqualTo(Integer.MIN_VALUE, 6);
+		assertThat(WRONG_RETURN, result == Integer.MIN_VALUE);
+	}
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkEqualTo(int, int)} method functions correctly
-	 * when the {@code x} argument is less than the {@code y} argument. The test will only
-	 * pass if an IllegalArgumentException is thrown.
+	 * when the {@code x} argument is less than the {@code y} argument. The test will only pass
+	 * if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckEqualTo_xLessThanY() {
 		IntChecker.checkEqualTo(5, 6);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkEqualTo(int, int, String)} method functions
-	 * correctly when the {@code x} argument is equal to the {@code y} argument. The test
-	 * will only pass if the correct integer is returned by the check and no exception is thrown.
+	 * correctly when the {@code x} argument is equal to the {@code y} argument. The test will
+	 * only pass if the correct integer is returned by the check and no exception is thrown.
 	 */
 	@Test
 	public void testCheckEqualTo_xEqualToY() {
 		final int result = IntChecker.checkEqualTo(5, 5);
 		assertThat(WRONG_RETURN, result == 5);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkEqualTo(int, int)} method functions correctly
-	 * when the {@code x} argument is greater than the {@code y} argument. The test will
-	 * only pass if an IllegalArgumentException is thrown.
+	 * when the {@code x} argument is greater than the {@code y} argument. The test will only
+	 * pass if an IllegalArgumentException is thrown.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testCheckEqualTo_xGreaterThanY() {
 		IntChecker.checkEqualTo(6, 5);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkNotEqualTo(int, int)} method functions
 	 * correctly when the {@code x} argument is less than the {@code notEqualTo} argument. The
@@ -137,7 +245,7 @@ public class TestIntChecker {
 		final int result = IntChecker.checkNotEqualTo(5, 6);
 		assertThat(WRONG_RETURN, result == 5);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkNotEqualTo(int, int)} method functions
 	 * correctly when the {@code x} argument is equal to the {@code notEqualTo} argument. The test
@@ -147,7 +255,7 @@ public class TestIntChecker {
 	public void testCheckNotEqualTo_xEqualToY() {
 		IntChecker.checkNotEqualTo(5, 5);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkNotEqualTo(int, int)} method functions
 	 * correctly when the {@code x} argument is greater than the {@code notEqualTo} argument. The
@@ -159,7 +267,7 @@ public class TestIntChecker {
 		final int result = IntChecker.checkNotEqualTo(6, 5);
 		assertThat(WRONG_RETURN, result == 6);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkBetween(int, int, int)} method functions
 	 * correctly when the {@code x} argument is less than the {@code lower} argument. The test
@@ -169,7 +277,7 @@ public class TestIntChecker {
 	public void testCheckBetween_xLessThanLower() {
 		IntChecker.checkBetween(5, 6, 8);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkBetween(int, int, int)} method functions
 	 * correctly when the {@code x} argument is equal to the {@code lower} argument. The test will
@@ -180,7 +288,7 @@ public class TestIntChecker {
 		final int result = IntChecker.checkBetween(6, 6, 8);
 		assertThat(WRONG_RETURN, result == 6);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkBetween(int, int, int)} method functions
 	 * correctly when the {@code x} argument is between the {@code lower} argument and the {@code
@@ -192,7 +300,7 @@ public class TestIntChecker {
 		final int result = IntChecker.checkBetween(7, 6, 8);
 		assertThat(WRONG_RETURN, result == 7);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkBetween(int, int, int)} method functions
 	 * correctly when the {@code x} argument is equal to the {@code upper} argument. The test will
@@ -203,7 +311,7 @@ public class TestIntChecker {
 		final int result = IntChecker.checkBetween(8, 6, 8);
 		assertThat(WRONG_RETURN, result == 8);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link IntChecker#checkBetween(int, int, int)} method functions
 	 * correctly when the {@code x} argument is greater than the {@code upper} argument. The test
