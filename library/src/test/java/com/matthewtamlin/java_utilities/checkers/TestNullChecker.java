c36154337237703896287edbbeb95b6418562fd8
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-25 08:51:25 +1030

    Updated TestNullChecker.java
    Added tests for checkEachElementIsNonNull methods

--- library/src/test/java/com/matthewtamlin/java_utilities/checkers/TestNullChecker.java
+++ library/src/test/java/com/matthewtamlin/java_utilities/checkers/TestNullChecker.java
@@ -16,9 +16,13 @@
 
 package com.matthewtamlin.java_utilities.checkers;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,7 +35,21 @@ public class TestNullChecker {
 	 * A string for use in testing.
 	 */
 	private static final String TEST_STRING = "test";
-
+	
+	private Collection<String> containingNull = new ArrayList<>();
+	
+	private Collection<String> notContainingNull = new ArrayList<>();
+	
+	@Before
+	public void setup() {
+		containingNull.clear();
+		containingNull.add(null);
+		containingNull.add(TEST_STRING);
+		
+		notContainingNull.clear();
+		notContainingNull.add(TEST_STRING);
+	}
+	
 	/**
 	 * Test to verify that the {@link NullChecker#checkNonNull(Object)} method functions correctly
 	 * when null is passed for the {@code object} argument. The test will only pass if an
@@ -41,7 +59,7 @@ public class TestNullChecker {
 	public void testCheckNonNull_1_nullPassed() {
 		NullChecker.checkNonNull(null);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link NullChecker#checkNonNull(Object)} method functions correctly
 	 * when a non-null String is passed for the {@code object} argument. The test will only pass if
@@ -53,7 +71,7 @@ public class TestNullChecker {
 		final String result = NullChecker.checkNonNull(TEST_STRING);
 		assertThat("incorrect object returned", result == TEST_STRING);
 	}
-
+	
 	/**
 	 * Test to verify that the {@link NullChecker#checkNonNull(Object, String)} method functions
 	 * correctly when null is passed for the {@code object} argument. The test will only pass if an
@@ -63,7 +81,7 @@ public class TestNullChecker {
 	public void testCheckNonNull_2_nullPassed() {
 		NullChecker.checkNonNull(null, "error message");
 	}
-
+	
 	/**
 	 * Test to verify that the {@link NullChecker#checkNonNull(Object, String)} method functions
 	 * correctly when a non-null String is passed for the {@code object} argument. The test will
@@ -74,5 +92,69 @@ public class TestNullChecker {
 	public void testCheckNonNull_2_nonNullPassed() {
 		final String result = NullChecker.checkNonNull(TEST_STRING, "error message");
 		assertThat("incorrect object returned", result == TEST_STRING);
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection)} method
+	 * functions correctly when a collection containing at least one null element is supplied.
+	 * The test will only pass if an IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckEachElementIsNonNull_1_collectionContainingNull() {
+		NullChecker.checkEachElementIsNonNull(containingNull);
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection)} method
+	 * functions correctly when a collection containing no null elements is supplied. The test
+	 * will only pass if the collection is returned.
+	 */
+	@Test
+	public void testCheckEachElementIsNonNull_1_collectionNotContainingNull() {
+		final Collection<String> result = NullChecker.checkEachElementIsNonNull(notContainingNull);
+		assertThat("incorrect collection returned", result == notContainingNull);
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection)} method
+	 * functions correctly when null is supplied. The test will only pass if an
+	 * IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckEachElementIsNonNull_1_nullSupplied() {
+		NullChecker.checkEachElementIsNonNull(null);
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection, String)}
+	 * method functions correctly when a collection containing at least one null element is
+	 * supplied for the {@code collection} argument. The test will only pass if an
+	 * IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckEachElementIsNonNull_2_collectionContainingNull() {
+		NullChecker.checkEachElementIsNonNull(containingNull, "error message");
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection, String)}
+	 * method functions correctly when a collection containing no null elements is supplied. The
+	 * test will only pass if the collection is returned.
+	 */
+	@Test
+	public void testCheckEachElementIsNonNull_2_collectionNotContainingNull() {
+		final Collection<String> result = NullChecker.checkEachElementIsNonNull(notContainingNull,
+				"error message");
+		assertThat("incorrect collection returned", result == notContainingNull);
+	}
+	
+	/**
+	 * Test to verify that the {@link NullChecker#checkEachElementIsNonNull(Collection)} method
+	 * functions correctly when null is supplied for the {@code collection} argument. The test will
+	 * only pass if an IllegalArgumentException is thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckEachElementIsNonNull_2_nullSupplied() {
+		NullChecker.checkEachElementIsNonNull(null, null);
 	}
 }
